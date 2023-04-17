b7302017007992d6f72455f3e617b0eca111c7f6
Alexander af Trolle <alexander.af.trolle@gmail.com>
2023-04-17 09:53:05 +0200

    Refactor Homescreen layout

--- shared/src/commonMain/kotlin/dev/trolle/af/wingman/compose/Home.kt
+++ shared/src/commonMain/kotlin/dev/trolle/af/wingman/compose/Home.kt
@@ -7,15 +7,16 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme.typography
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -33,61 +34,62 @@ fun Home(
     state: HomeState,
 ) = Scaffold {
     LazyColumn(
-        modifier = Modifier.systemBarsPadding(),
-        contentPadding = PaddingValues(horizontal = 8.dp, vertical = 8.dp)
+        contentPadding = PaddingValues(vertical = 32.dp) // TODO add systembar padding
     ) {
         items(
             count = state.matches.size,
             itemContent = { index ->
                 val match = state.matches[index]
                 Match(match)
-            })
-
+            }
+        )
     }
 }
 
 @Composable
 fun Match(match: Match) {
-    Card(
-        modifier = Modifier.fillMaxWidth().padding(vertical = 8.dp),
-        elevation = 2.dp,
-        shape = RoundedCornerShape(16.dp)
+    Column(
+        Modifier.padding(vertical = 8.dp)
     ) {
-        Column {
-            Row(
-                Modifier.height(96.dp)
-            ) {
-                Image(
-                    painter = rememberAsyncImagePainter(match.imageUrl),
-                    contentDescription = null,
-                    contentScale = ContentScale.Crop,
-                    modifier = Modifier
-                        .padding(8.dp)
-                        .fillMaxHeight()
-                        .aspectRatio(1f)
-                        .clip(RoundedCornerShape(16.dp))
-                )
+        Row(
+            Modifier.height(96.dp)
+                .padding(horizontal = 8.dp)
+        ) {
+            Image(
+                painter = rememberAsyncImagePainter(match.imageUrl),
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .padding(8.dp)
+                    .fillMaxHeight()
+                    .aspectRatio(1f)
+                    .clip(RoundedCornerShape(16.dp))
+            )
 
+            Column(Modifier.align(Alignment.Top)) {
                 Text(
-                    modifier = Modifier
-                        .align(Alignment.CenterVertically)
-                        .padding(8.dp),
                     text = match.name,
                     style = typography.h4,
                     textAlign = TextAlign.Start
                 )
-            }
-
-            Box(
-                modifier = Modifier
-            ) {
                 Text(
-                    modifier = Modifier.padding(start = 8.dp, end = 8.dp, bottom = 8.dp),
-                    text = match.opener,
-                    style = typography.body2
+                    text = match.age,
+                    style = typography.h6,
+                    textAlign = TextAlign.Start
                 )
             }
         }
+
+        Box(Modifier.padding(horizontal = 8.dp)) {
+            BasicTextField(
+                modifier = Modifier.padding(start = 8.dp, end = 8.dp, bottom = 8.dp),
+                value = match.opener,
+                readOnly = true,
+                onValueChange = { value: String -> },
+                textStyle = typography.body2,
+            )
+        }
+        Divider()
     }
 
 }
